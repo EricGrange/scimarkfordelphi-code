1f8135a1b9028f28978f34e34963dabd509237a1
delphifreak71 <delphifreak71@0b7119d2-0da0-430f-a178-dca91777aa72>
2020-05-17 14:53:08 +0000

    - added a binary compiled with trunk-version of FPC 3.3.1

--- bin/Run_All.bat
+++ bin/Run_All.bat
@@ -26,4 +26,5 @@ scimark_codetyphon_59_fpc_311_win32_Optimization_O4_Revision_10.exe > .\win32\sc
 scimark_codetyphon_59_fpc_311_win32_Optimization_O1_Revision_10.exe > .\win32\scimark_codetyphon_59_fpc_311_win32_Optimization_O1_Revision_10.txt
 scimark_D_XE10_1_Berlin_Update2_win32.exe > .\win32\scimark_D_XE10_1_Berlin_Update2_win32.txt
 Scimark_2.0.0RC2_r59373_FPC_3.0.4_win32_Optimization_O1.exe >.\win32\Scimark_2.0.0RC2_r59373_FPC_3.0.4_win32_Optimization_O1.txt
+Scimark_Lazarus_2_1_0_r63175_FPC_3_3_1_x86_64-win64_O2.exe >.\win64\Scimark_Lazarus_2_1_0_r63175_FPC_3_3_1_x86_64-win64_O2.txt
 pause
